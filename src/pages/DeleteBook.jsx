a10b3ee533d481823154fb4a01ad1ff6f7ac229b
mrkrezza1 <sajit1020@gmail.com>
2024-07-22 17:39:59 +0530

    here we have fix the issue of fetching from the backend

--- src/pages/DeleteBook.jsx
+++ src/pages/DeleteBook.jsx
@@ -27,7 +27,7 @@ const DeleteBook = () => {
     <div className='p-4'>
       <BackButton />
       <h1 className='text-3xl my-4'>Delete Book</h1>
-      {loading ? <Spinner /> : ""}
+      <div className=' w-full flex items-center justify-center'>{loading ? <Spinner /> : ""}</div>
       <div className='flex flex-col items-center border-2 border-sky-400 rounded-xl width-[600px] p-8 mx-auto'>
         <h3 className='text-2xl'>Are you sure want to delete this book?</h3>
         <button className='p-4 bg-red-600 text-white m-8 w-full' onClick={handleDeleteButton}>
